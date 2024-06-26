38c2a48d8b01f629ef4425196848d307f82ecaa5
Yvan Sugira <phoenixnavyn7@gmail.com>
2024-06-26 07:40:31 +0200

    [feat] Final Commit

--- src/main/java/com/navyn/SpringBootStarter/BnrBankingApplication.java
+++ src/main/java/com/navyn/SpringBootStarter/BnrBankingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootStarterApplication {
+public class BnrBankingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStarterApplication.class, args);
+        SpringApplication.run(BnrBankingApplication.class, args);
     }
 }
